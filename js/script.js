6632712d671e4527dc33f6819d3785327c385bf5
luiz <conttatoluiz@gmail.com>
2022-07-13 20:49:30 -0300

    Finalizando o jogo

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ const jump = () => {
 }
 const loop = setInterval(() => {
 
+    console.log("loop")
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "" );
 
@@ -22,6 +24,15 @@ const loop = setInterval(() => {
 
         pipe.style.animation = "none";
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "./images/perdeu.png";
+        mario.style.width  = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
 
     }
 
